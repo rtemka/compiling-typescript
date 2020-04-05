7a3a2027e4f46b614e666451139a23ee47cd8cf1
Artem Rybakov <gizmo3000@yandex.ru>
2020-04-05 17:13:33 +0300

    add functionality

---
+++ @types/Question.d.ts
@@ -0,0 +1,7 @@
+export interface Question {
+
+    title: string,
+    content: string,
+    answerCount?: number
+
+}
--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,13 @@
 import * as express from 'express';
+import * as _ from 'lodash';
+import * as path from 'path';
+import { Question } from '../@types/Question'
+
+const questions : Question[] = [{
+    title: "How to log in?",
+    content: "How do I log in?",
+    answerCount: 2
+}]
 
 const port : string | number = process.env.port || 1337;
 
@@ -8,3 +17,15 @@ app.listen(port);
 app.use(express.static('public'));
 
 console.log("Listening on port " + port);
+
+app.get("/questions", (_req, res) => {
+
+    res.json(questions);
+
+})
+
+app.get("/main.js", (_req, res) => {
+
+    res.sendFile(path.resolve(__dirname, "..", "client", "client.js"));
+
+})
