a84e668ff73f6cc3677b55661e615812d60b845b
Artem Rybakov <gizmo3000@yandex.ru>
2020-04-05 14:45:17 +0300

    Initial commit

---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+
+const port : string | number = process.env.port || 1337;
+
+const app = express();
+
+app.listen(port);
+app.use(express.static('public'));
+
+console.log("Listening on port " + port);
